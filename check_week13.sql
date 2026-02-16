0462942097da1d62ab0597202e7b177a79e003a5
coltItalo <cgnech33@gmail.com>
2026-02-16 14:43:22 -0800

    Clean up: Remove temporary check scripts, old test files, and obsolete deployment guides

--- check_week13.sql
+++
@@ -1,1 +0,0 @@
-SELECT week_number, roster_unlock_time FROM web_week WHERE week_number=13;
--- query_trades.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /opt/shamrock-fantasy
-sqlite3 db.sqlite3 << EOF
-SELECT id, status, executed_at, proposing_team_id, receiving_team_id FROM web_trade ORDER BY created_at DESC LIMIT 20;
-EOF
