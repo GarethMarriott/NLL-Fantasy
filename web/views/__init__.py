79154bb116f52a21443c428b6f68e20ca5c043ea
coltItalo <cgnech33@gmail.com>
2026-02-16 21:00:14 -0800

    FIX: Remove caching from team_detail view
    The @cache_view_result decorator was incompatible with team_detail because:
    - The cache key function requires (team_id, week_num)
    - team_id comes from URL parameter (*args)
    - week_num comes from request.GET parameters
    
    Removed the decorator to allow the view to function properly.
    Team detail page will now work without caching.

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -219,7 +219,6 @@ def about(request):
     return render(request, "web/about.html")
 
 
-@cache_view_result(get_team_detail_cache_key, 'team_detail')
 def team_detail(request, team_id):
     team = get_object_or_404(Team, id=team_id)
     
