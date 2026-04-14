5dc37c20aae3a7af5a3a15e9956b27c3a64c7cd1
coltItalo <cgnech33@gmail.com>
2026-04-13 17:57:54 -0700

    Fix: Rename migration to 0066 and update dependencies to follow 0065

--- web/migrations/0066_add_offseason_features.py
+++ web/migrations/0066_add_offseason_features.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('web', '0027_league_use_waivers_waiverclaim'),
+        ('web', '0065_add_game_scores'),
     ]
 
     operations = [
