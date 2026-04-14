d3d00f69b71eede1dec7bb5f14e0cf4d8a945f54
coltItalo <cgnech33@gmail.com>
2026-04-13 20:35:27 -0700

    Fix Chrome headless mode on production server

--- web/management/commands/scrape_nll_transactions.py
+++ web/management/commands/scrape_nll_transactions.py
@@ -25,7 +25,10 @@ def scrape_nll_transactions_task(headless=True):
         if headless:
             chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument('--single-process')
         chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
         
         # Initialize driver
